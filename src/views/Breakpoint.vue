d1c91baa7a02f9719fce453d97c4a96683cfa650
dongkyukim <dongkyukim@mz.co.kr>
2022-07-07 18:27:55 +0900

    ddd

--- src/views/Breakpoint.vue
+++ src/views/Breakpoint.vue
@@ -73,7 +73,7 @@
           $vuetify.breakpoint.mobile:{{$vuetify.breakpoint.mobile}}<br />
           $vuetify.breakpoint.mobile:{{$vuetify.breakpoint.mobile}}<br />
           $vuetify.breakpoint.mobile:{{$vuetify.breakpoint.mobile}}
-11111122224444222222223333111122222
+11111122224444222222223333111122222ddddd
         </v-card>
         <v-card>1111</v-card>
       </v-container>
